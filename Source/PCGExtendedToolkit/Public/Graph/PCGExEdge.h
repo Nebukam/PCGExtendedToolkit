955427ffa523603c51e3ef01cc1714817472ffd0
Tim <nebukam@gmail.com>
2023-12-11 00:57:10 -0500

    WIP

---
+++ Source/PCGExtendedToolkit/Public/Graph/PCGExEdge.h
@@ -0,0 +1,96 @@
+﻿// Copyright Timothé Lapetite 2023
+// Released under the MIT license https://opensource.org/license/MIT/
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Data/PCGExPointIO.h"
+#include "Data/PCGExAttributeHelpers.h"
+#include "PCGExMT.h"
+
+#include "PCGExEdge.generated.h"
+
+UENUM(BlueprintType, meta=(Bitflags, UseEnumValuesAsMaskValuesInEditor="true"))
+enum class EPCGExEdgeType : uint8
+{
+	Unknown  = 0 UMETA(DisplayName = "Unknown", Tooltip="Unknown type."),
+	Roaming  = 1 << 0 UMETA(DisplayName = "Roaming", Tooltip="Unidirectional edge."),
+	Shared   = 1 << 1 UMETA(DisplayName = "Shared", Tooltip="Shared edge, both sockets are connected; but do not match."),
+	Match    = 1 << 2 UMETA(DisplayName = "Match", Tooltip="Shared relation, considered a match by the primary socket owner; but does not match on the other."),
+	Complete = 1 << 3 UMETA(DisplayName = "Complete", Tooltip="Shared, matching relation on both sockets."),
+	Mirror   = 1 << 4 UMETA(DisplayName = "Mirrored relation", Tooltip="Mirrored relation, connected sockets are the same on both points."),
+};
+
+namespace PCGExGraph
+{
+
+	const FName SourceEdgesLabel = TEXT("Edges");
+	const FName OutputEdgesLabel = TEXT("Edges");
+
+	struct PCGEXTENDEDTOOLKIT_API FEdge
+	{
+		uint32 Start = 0;
+		uint32 End = 0;
+		EPCGExEdgeType Type = EPCGExEdgeType::Unknown;
+
+		FEdge()
+		{
+		}
+
+		FEdge(const int32 InStart, const int32 InEnd, const EPCGExEdgeType InType):
+			Start(InStart), End(InEnd), Type(InType)
+		{
+		}
+
+		bool operator==(const FEdge& Other) const
+		{
+			return Start == Other.Start && End == Other.End;
+		}
+
+		explicit operator uint64() const
+		{
+			return static_cast<uint64>(Start) | (static_cast<uint64>(End) << 32);
+		}
+
+		explicit FEdge(const uint64 InValue)
+		{
+			Start = static_cast<uint32>(InValue & 0xFFFFFFFF);
+			End = static_cast<uint32>((InValue >> 32) & 0xFFFFFFFF);
+			Type = EPCGExEdgeType::Unknown;
+		}
+	};
+
+	struct PCGEXTENDEDTOOLKIT_API FUnsignedEdge : public FEdge
+	{
+		FUnsignedEdge(): FEdge()
+		{
+		}
+
+		FUnsignedEdge(const int32 InStart, const int32 InEnd, const EPCGExEdgeType InType):
+			FEdge(InStart, InEnd, InType)
+		{
+		}
+
+		bool operator==(const FUnsignedEdge& Other) const
+		{
+			return GetUnsignedHash() == Other.GetUnsignedHash();
+		}
+
+		explicit FUnsignedEdge(const uint64 InValue)
+		{
+			Start = static_cast<uint32>(InValue & 0xFFFFFFFF);
+			End = static_cast<uint32>((InValue >> 32) & 0xFFFFFFFF);
+			Type = EPCGExEdgeType::Unknown;
+		}
+
+		uint64 GetUnsignedHash() const
+		{
+			return Start > End ?
+				       static_cast<uint64>(Start) | (static_cast<uint64>(End) << 32) :
+				       static_cast<uint64>(End) | (static_cast<uint64>(Start) << 32);
+		}
+	};
+
+#pragma endregion
+}
--- Source/PCGExtendedToolkit/Public/Graph/PCGExGraph.h
+++ Source/PCGExtendedToolkit/Public/Graph/PCGExGraph.h
@@ -8,19 +8,9 @@
 #include "Data/PCGExPointIO.h"
 #include "Data/PCGExAttributeHelpers.h"
 #include "PCGExMT.h"
+#include "PCGExEdge.h"
 
 #include "PCGExGraph.generated.h"
-
-UENUM(BlueprintType, meta=(Bitflags, UseEnumValuesAsMaskValuesInEditor="true"))
-enum class EPCGExEdgeType : uint8
-{
-	Unknown  = 0 UMETA(DisplayName = "Unknown", Tooltip="Unknown type."),
-	Roaming  = 1 << 0 UMETA(DisplayName = "Roaming", Tooltip="Unidirectional edge."),
-	Shared   = 1 << 1 UMETA(DisplayName = "Shared", Tooltip="Shared edge, both sockets are connected; but do not match."),
-	Match    = 1 << 2 UMETA(DisplayName = "Match", Tooltip="Shared relation, considered a match by the primary socket owner; but does not match on the other."),
-	Complete = 1 << 3 UMETA(DisplayName = "Complete", Tooltip="Shared, matching relation on both sockets."),
-	Mirror   = 1 << 4 UMETA(DisplayName = "Mirrored relation", Tooltip="Mirrored relation, connected sockets are the same on both points."),
-};
 
 ENUM_CLASS_FLAGS(EPCGExEdgeType)
 
@@ -290,9 +280,6 @@ namespace PCGExGraph
 
 	const FName SourceGraphsLabel = TEXT("In");
 	const FName OutputGraphsLabel = TEXT("Out");
-
-	const FName SourceEdgesLabel = TEXT("Edges");
-	const FName OutputEdgesLabel = TEXT("Edges");
 
 	const FName SourcePathsLabel = TEXT("Paths");
 	const FName OutputPathsLabel = TEXT("Paths");
@@ -669,70 +656,6 @@ namespace PCGExGraph
 #pragma endregion
 
 #pragma region Edges
-
-	struct PCGEXTENDEDTOOLKIT_API FEdge
-	{
-		uint32 Start = 0;
-		uint32 End = 0;
-		EPCGExEdgeType Type = EPCGExEdgeType::Unknown;
-
-		FEdge()
-		{
-		}
-
-		FEdge(const int32 InStart, const int32 InEnd, const EPCGExEdgeType InType):
-			Start(InStart), End(InEnd), Type(InType)
-		{
-		}
-
-		bool operator==(const FEdge& Other) const
-		{
-			return Start == Other.Start && End == Other.End;
-		}
-
-		explicit operator uint64() const
-		{
-			return static_cast<uint64>(Start) | (static_cast<uint64>(End) << 32);
-		}
-
-		explicit FEdge(const uint64 InValue)
-		{
-			Start = static_cast<uint32>(InValue & 0xFFFFFFFF);
-			End = static_cast<uint32>((InValue >> 32) & 0xFFFFFFFF);
-			Type = EPCGExEdgeType::Unknown;
-		}
-	};
-
-	struct PCGEXTENDEDTOOLKIT_API FUnsignedEdge : public FEdge
-	{
-		FUnsignedEdge(): FEdge()
-		{
-		}
-
-		FUnsignedEdge(const int32 InStart, const int32 InEnd, const EPCGExEdgeType InType):
-			FEdge(InStart, InEnd, InType)
-		{
-		}
-
-		bool operator==(const FUnsignedEdge& Other) const
-		{
-			return GetUnsignedHash() == Other.GetUnsignedHash();
-		}
-
-		explicit FUnsignedEdge(const uint64 InValue)
-		{
-			Start = static_cast<uint32>(InValue & 0xFFFFFFFF);
-			End = static_cast<uint32>((InValue >> 32) & 0xFFFFFFFF);
-			Type = EPCGExEdgeType::Unknown;
-		}
-
-		uint64 GetUnsignedHash() const
-		{
-			return Start > End ?
-				       static_cast<uint64>(Start) | (static_cast<uint64>(End) << 32) :
-				       static_cast<uint64>(End) | (static_cast<uint64>(Start) << 32);
-		}
-	};
 
 	struct PCGEXTENDEDTOOLKIT_API FCachedSocketData
 	{
