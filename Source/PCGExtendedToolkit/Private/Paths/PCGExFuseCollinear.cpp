ef5ebe997dda319a5ae80ae06bd4c68db96cd222
Tim <nebukam@gmail.com>
2023-12-18 23:00:50 -0500

    WIP

--- Source/PCGExtendedToolkit/Private/Paths/PCGExFuseCollinear.cpp
+++ Source/PCGExtendedToolkit/Private/Paths/PCGExFuseCollinear.cpp
@@ -1,1 +1,119 @@
-﻿
+﻿// Copyright Timothé Lapetite 2023
+// Released under the MIT license https://opensource.org/license/MIT/
+
+#include "Paths/PCGExFuseCollinear.h"
+
+#define LOCTEXT_NAMESPACE "PCGExFuseCollinearElement"
+#define PCGEX_NAMESPACE FuseCollinear
+
+UPCGExFuseCollinearSettings::UPCGExFuseCollinearSettings(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	//PCGEX_DEFAULT_OPERATION(Blending, UPCGExSubPointsBlendInterpolate)
+}
+
+void UPCGExFuseCollinearSettings::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	//if (Blending) { Blending->UpdateUserFacingInfos(); }
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+
+PCGExData::EInit UPCGExFuseCollinearSettings::GetMainOutputInitMode() const { return PCGExData::EInit::NewOutput; }
+
+FPCGElementPtr UPCGExFuseCollinearSettings::CreateElement() const { return MakeShared<FPCGExFuseCollinearElement>(); }
+
+FPCGExFuseCollinearContext::~FPCGExFuseCollinearContext()
+{
+	PCGEX_TERMINATE_ASYNC
+}
+
+PCGEX_INITIALIZE_CONTEXT(FuseCollinear)
+
+bool FPCGExFuseCollinearElement::Boot(FPCGContext* InContext) const
+{
+	if (!FPCGExPathProcessorElement::Boot(InContext)) { return false; }
+
+	PCGEX_CONTEXT_AND_SETTINGS(FuseCollinear)
+
+	Context->Threshold = FMath::Cos(Settings->Threshold * (PI / 180.0));
+
+	//PCGEX_FWD(bDoBlend)
+	//PCGEX_BIND_OPERATION(Blending, UPCGExSubPointsBlendInterpolate)
+
+	return true;
+}
+
+
+bool FPCGExFuseCollinearElement::ExecuteInternal(FPCGContext* InContext) const
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(FPCGExFuseCollinearElement::Execute);
+
+	PCGEX_CONTEXT(FuseCollinear)
+
+	if (Context->IsSetup())
+	{
+		if (!Boot(Context)) { return true; }
+		Context->SetState(PCGExMT::State_ReadyForNextPoints);
+	}
+
+	if (Context->IsState(PCGExMT::State_ReadyForNextPoints))
+	{
+		int32 Index = 0;
+		while (Context->AdvancePointsIO())
+		{
+			if (Context->CurrentIO->GetNum() > 2)
+			{
+				//Context->CurrentIO->CreateInKeys();
+				Context->GetAsyncManager()->Start<FFuseCollinearTask>(Index++, Context->CurrentIO);
+			}
+		}
+		Context->SetAsyncState(PCGExMT::State_WaitingOnAsyncWork);
+	}
+
+	if (Context->IsState(PCGExMT::State_WaitingOnAsyncWork))
+	{
+		if (Context->IsAsyncWorkComplete()) { Context->Done(); }
+	}
+
+	if (Context->IsDone())
+	{
+		Context->OutputPoints();
+	}
+
+	return Context->IsDone();
+}
+
+bool FFuseCollinearTask::ExecuteTask()
+{
+	const FPCGExFuseCollinearContext* Context = Manager->GetContext<FPCGExFuseCollinearContext>();
+
+	const TArray<FPCGPoint>& InPoints = PointIO->GetIn()->GetPoints();
+	TArray<FPCGPoint>& OutPoints = PointIO->GetOut()->GetMutablePoints();
+	OutPoints.Add_GetRef(InPoints[0]);
+
+	FVector CurrentDirection = (InPoints[0].Transform.GetLocation() - InPoints[1].Transform.GetLocation()).GetSafeNormal();
+	const int32 MaxIndex = InPoints.Num() - 1;
+
+	for (int i = 1; i < MaxIndex; i++)
+	{
+		FVector CurrentPosition = InPoints[i].Transform.GetLocation();
+		FVector NextPosition = InPoints[i + 1].Transform.GetLocation();
+		FVector DirToNext = (CurrentPosition - NextPosition).GetSafeNormal();
+
+		if (FVector::DotProduct(CurrentDirection, DirToNext) > Context->Threshold)
+		{
+			// Collinear with previous, keep moving
+			continue;
+		}
+
+		OutPoints.Add_GetRef(InPoints[i]);
+		CurrentDirection = DirToNext;
+	}
+
+	OutPoints.Add_GetRef(InPoints[MaxIndex]);
+
+	return true;
+}
+
+#undef LOCTEXT_NAMESPACE
+#undef PCGEX_NAMESPACE
--- Source/PCGExtendedToolkit/Public/Paths/PCGExFuseCollinear.h
+++ Source/PCGExtendedToolkit/Public/Paths/PCGExFuseCollinear.h
@@ -1,1 +1,78 @@
-﻿
+﻿// Copyright Timothé Lapetite 2023
+// Released under the MIT license https://opensource.org/license/MIT/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PCGExPathProcessor.h"
+#include "PCGExPointsProcessor.h"
+#include "PCGExFuseCollinear.generated.h"
+
+/**
+ * Calculates the distance between two points (inherently a n*n operation)
+ */
+UCLASS(BlueprintType, ClassGroup = (Procedural), Category="PCGEx|Path")
+class PCGEXTENDEDTOOLKIT_API UPCGExFuseCollinearSettings : public UPCGExPathProcessorSettings
+{
+	GENERATED_BODY()
+
+public:
+	UPCGExFuseCollinearSettings(const FObjectInitializer& ObjectInitializer);
+
+	//~Begin UPCGSettings interface
+#if WITH_EDITOR
+	PCGEX_NODE_INFOS(FuseCollinear, "Path : Fuse Collinear", "FuseCollinear paths points.");
+#endif
+
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	virtual PCGExData::EInit GetMainOutputInitMode() const override;
+
+protected:
+	virtual FPCGElementPtr CreateElement() const override;
+	//~End UPCGSettings interface
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_Overridable, Units="Degrees", ClampMin=0, ClampMax=180))
+	double Threshold = 10;
+
+	//UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_Overridable, InlineEditConditionToggle))
+	//bool bDoBlend = false;
+
+	//UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, Instanced, meta=(PCG_Overridable, NoResetToDefault, ShowOnlyInnerProperties, EditCondition="bDoBlend"))
+	//TObjectPtr<UPCGExSubPointsBlendOperation> Blending = nullptr;
+};
+
+struct PCGEXTENDEDTOOLKIT_API FPCGExFuseCollinearContext : public FPCGExPathProcessorContext
+{
+	friend class FPCGExFuseCollinearElement;
+
+	virtual ~FPCGExFuseCollinearContext() override;
+
+	double Threshold;
+	//bool bDoBlend;
+	//UPCGExSubPointsBlendOperation* Blending = nullptr;
+};
+
+class PCGEXTENDEDTOOLKIT_API FPCGExFuseCollinearElement : public FPCGExPathProcessorElement
+{
+public:
+	virtual FPCGContext* Initialize(
+		const FPCGDataCollection& InputData,
+		TWeakObjectPtr<UPCGComponent> SourceComponent,
+		const UPCGNode* Node) override;
+
+protected:
+	virtual bool Boot(FPCGContext* InContext) const override;
+	virtual bool ExecuteInternal(FPCGContext* Context) const override;
+};
+
+class PCGEXTENDEDTOOLKIT_API FFuseCollinearTask : public FPCGExNonAbandonableTask
+{
+public:
+	FFuseCollinearTask(FPCGExAsyncManager* InManager, const int32 InTaskIndex, PCGExData::FPointIO* InPointIO) :
+		FPCGExNonAbandonableTask(InManager, InTaskIndex, InPointIO)
+	{
+	}
+
+	virtual bool ExecuteTask() override;
+};
