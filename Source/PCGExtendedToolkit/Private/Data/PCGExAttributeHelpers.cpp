5be3863fe06f533864d796f14813b143220edf10
Tim <nebukam@gmail.com>
2024-04-28 15:53:31 -0400

    Update PCGExAttributeHelpers.cpp
    Removed #pragma-once in main, update #36

--- Source/PCGExtendedToolkit/Private/Data/PCGExAttributeHelpers.cpp
+++ Source/PCGExtendedToolkit/Private/Data/PCGExAttributeHelpers.cpp
@@ -1,8 +1,6 @@
 ﻿// Copyright Timothé Lapetite 2024
 // Released under the MIT license https://opensource.org/license/MIT/
 
-#pragma once
-
 #include "Data/PCGExAttributeHelpers.h"
 
 #if WITH_EDITOR
