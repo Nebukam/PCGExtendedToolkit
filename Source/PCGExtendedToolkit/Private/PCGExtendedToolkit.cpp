b894c0ac542179312d67f384c4b7504625c01f4e
Tim <nebukam@gmail.com>
2024-07-09 23:24:36 -0400

    Update PCGExtendedToolkit.cpp

--- Source/PCGExtendedToolkit/Private/PCGExtendedToolkit.cpp
+++ Source/PCGExtendedToolkit/Private/PCGExtendedToolkit.cpp
@@ -2,8 +2,9 @@
 // Released under the MIT license https://opensource.org/license/MIT/
 
 #include "PCGExtendedToolkit.h"
-
+#if WITH_EDITOR
 #include "ISettingsModule.h"
+#endif
 #include "PCGExGlobalSettings.h"
 
 #define LOCTEXT_NAMESPACE "FPCGExtendedToolkitModule"
@@ -11,6 +12,7 @@
 void FPCGExtendedToolkitModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+#if WITH_EDITOR
 	if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
 	{
 		SettingsModule->RegisterSettings(
@@ -20,16 +22,19 @@ void FPCGExtendedToolkitModule::StartupModule()
 				GetMutableDefault<UPCGExGlobalSettings>()
 			);
 	}
+#endif
 }
 
 void FPCGExtendedToolkitModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module
+#if WITH_EDITOR
 	if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
 	{
 		SettingsModule->UnregisterSettings("Project", "Plugins", "PCGEx");
 	}
+#endif
 }
 
 #undef LOCTEXT_NAMESPACE
