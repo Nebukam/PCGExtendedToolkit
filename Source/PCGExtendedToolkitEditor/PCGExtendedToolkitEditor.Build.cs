e6f86b28309876de9d4263590727138f9f283c2d
Tim <nebukam@gmail.com>
2024-10-24 02:31:34 -0400

    Added icons

---
+++ Source/PCGExtendedToolkitEditor/PCGExtendedToolkitEditor.Build.cs
@@ -0,0 +1,31 @@
+﻿using UnrealBuildTool;
+
+public class PCGExtendedToolkitEditor : ModuleRules
+{
+    public PCGExtendedToolkitEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "UnrealEd", 
+                "Slate", 
+                "SlateCore",
+                "EditorStyle",
+                "Projects"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/PCGExtendedToolkitEditor/Public/PCGExtendedToolkitEditor.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+class FPCGExtendedToolkitEditorModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+};
