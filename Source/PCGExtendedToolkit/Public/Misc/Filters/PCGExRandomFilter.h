9271230567e0d207005ac3d6b10e1b0ffaae9fc4
Tim <nebukam@gmail.com>
2024-12-23 13:41:58 -0500

    Day one patch

--- Source/PCGExtendedToolkit/Public/Misc/Filters/PCGExRandomFilter.h
+++ Source/PCGExtendedToolkit/Public/Misc/Filters/PCGExRandomFilter.h
@@ -47,7 +47,7 @@ struct /*PCGEXTENDEDTOOLKIT_API*/ FPCGExRandomFilterConfig
 
 
 	/** Whether to use in-editor curve or an external asset. */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_NotOverridable, DisplayPriority=-1))
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_NotOverridable))
 	bool bUseLocalCurve = false;
 
 	// TODO: DirtyCache for OnDependencyChanged when this float curve is an external asset
