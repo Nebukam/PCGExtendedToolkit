54303aecdfccef6b6cc9af9573a70d50cb9d0b11
Tim <nebukam@gmail.com>
2025-03-08 14:48:36 -0500

    New Asset Action : Create/Update Asset Collection helper

---
+++ Source/PCGExtendedToolkitEditor/Private/Collections/PCGExActorCollectionUtils.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright 2025 Timothé Lapetite and contributors
+// Released under the MIT license https://opensource.org/license/MIT/
+
+#include "Collections/PCGExActorCollectionUtils.h"
+
+#include "PCGExtendedToolkitEditor.h"
+#include "FileHelpers.h"
+#include "UObject/UObjectGlobals.h"
+#include "UObject/Package.h"
+
+void PCGExActorCollectionUtils::CreateCollectionFrom(const TArray<FAssetData>& SelectedAssets)
+{
+	if (SelectedAssets.IsEmpty()) { return; }
+
+	// TODO
+}
+
+void PCGExActorCollectionUtils::UpdateCollectionsFrom(
+	const TArray<TObjectPtr<UPCGExActorCollection>>& SelectedCollections,
+	const TArray<FAssetData>& SelectedAssets,
+	bool bIsNewCollection)
+{
+}
