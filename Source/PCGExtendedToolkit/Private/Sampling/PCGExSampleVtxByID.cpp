269eae0d8eefd3633b11e6dcc847232f70dbe941
Tim <nebukam@gmail.com>
2025-06-21 14:23:18 -0400

    WIP

---
+++ Source/PCGExtendedToolkit/Private/Sampling/PCGExSampleVtxByID.cpp
@@ -0,0 +1,292 @@
+﻿// Copyright 2025 Timothé Lapetite and contributors
+// Released under the MIT license https://opensource.org/license/MIT/
+
+#include "Sampling/PCGExSampleVtxByID.h"
+
+#include "PCGExPointsProcessor.h"
+#include "Data/Blending/PCGExBlendOpFactoryProvider.h"
+#include "Data/Blending/PCGExBlendOpsManager.h"
+
+
+#include "Misc/PCGExSortPoints.h"
+
+
+#define LOCTEXT_NAMESPACE "PCGExSampleVtxByIDElement"
+#define PCGEX_NAMESPACE SampleVtxByID
+
+UPCGExSampleVtxByIDSettings::UPCGExSampleVtxByIDSettings(
+	const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+TArray<FPCGPinProperties> UPCGExSampleVtxByIDSettings::InputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties = Super::InputPinProperties();
+
+	PCGEX_PIN_POINTS(PCGExGraph::SourceVerticesLabel, "The point data set to check against.", Required, {})
+	PCGEX_PIN_FACTORIES(PCGExDataBlending::SourceBlendingLabel, "Blending configurations, used by Individual (non-monolithic) blending interface.", Normal, {})
+
+	return PinProperties;
+}
+
+PCGEX_INITIALIZE_ELEMENT(SampleVtxByID)
+
+bool FPCGExSampleVtxByIDElement::Boot(FPCGExContext* InContext) const
+{
+	if (!FPCGExPointsProcessorElement::Boot(InContext)) { return false; }
+
+	PCGEX_CONTEXT_AND_SETTINGS(SampleVtxByID)
+
+	PCGEX_FWD(ApplySampling)
+	Context->ApplySampling.Init();
+
+	PCGExFactories::GetInputFactories<UPCGExBlendOpFactory>(
+		Context, PCGExDataBlending::SourceBlendingLabel, Context->BlendingFactories,
+		{PCGExFactories::EType::Blending}, false);
+
+	FBox OctreeBounds = FBox(ForceInit);
+
+	TSharedPtr<PCGExData::FPointIOCollection> Targets = MakeShared<PCGExData::FPointIOCollection>(
+		Context, PCGExGraph::SourceVerticesLabel, PCGExData::EIOInit::NoInit, true);
+
+	if (Targets->IsEmpty())
+	{
+		if (!Settings->bQuietMissingInputError) { PCGE_LOG_C(Error, GraphAndLog, InContext, FTEXT("No targets (empty datasets)")); }
+		return false;
+	}
+
+	for (const TSharedPtr<PCGExData::FPointIO>& IO : Targets->Pairs)
+	{
+		TSharedPtr<PCGExData::FFacade> TargetFacade = MakeShared<PCGExData::FFacade>(IO.ToSharedRef());
+		TargetFacade->Idx = Context->TargetFacades.Num();
+
+		Context->TargetFacades.Add(TargetFacade.ToSharedRef());
+	}
+
+	Context->TargetsPreloader = MakeShared<PCGExData::FMultiFacadePreloader>(Context->TargetFacades);
+
+	Context->TargetsPreloader->ForEach(
+		[&](PCGExData::FFacadePreloader& Preloader)
+		{
+			Preloader.Register<int64>(Context, PCGExGraph::Attr_PCGExVtxIdx);
+			PCGExDataBlending::RegisterBuffersDependencies_SourceA(Context, Preloader, Context->BlendingFactories);
+		});
+
+	return true;
+}
+
+void FPCGExSampleVtxByIDElement::PostLoadAssetsDependencies(FPCGExContext* InContext) const
+{
+	FPCGExPointsProcessorElement::PostLoadAssetsDependencies(InContext);
+
+	PCGEX_CONTEXT_AND_SETTINGS(SampleVtxByID)
+}
+
+bool FPCGExSampleVtxByIDElement::ExecuteInternal(FPCGContext* InContext) const
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(FPCGExSampleVtxByIDElement::Execute);
+
+	PCGEX_CONTEXT_AND_SETTINGS(SampleVtxByID)
+	PCGEX_EXECUTION_CHECK
+	PCGEX_ON_INITIAL_EXECUTION
+	{
+		Context->SetAsyncState(PCGEx::State_FacadePreloading);
+
+		TWeakPtr<FPCGContextHandle> WeakHandle = Context->GetOrCreateHandle();
+		Context->TargetsPreloader->OnCompleteCallback = [Settings, Context, WeakHandle]()
+		{
+			// TODO Build Idx :: <index|IO> lookup map
+
+			PCGEX_SHARED_CONTEXT_VOID(WeakHandle)
+
+			if (!Context->StartBatchProcessingPoints<PCGExPointsMT::TBatch<PCGExSampleVtxByIDs::FProcessor>>(
+				[&](const TSharedPtr<PCGExData::FPointIO>& Entry) { return true; },
+				[&](const TSharedPtr<PCGExPointsMT::TBatch<PCGExSampleVtxByIDs::FProcessor>>& NewBatch)
+				{
+					NewBatch->bRequiresWriteStep = Settings->bPruneFailedSamples;
+				}))
+			{
+				Context->CancelExecution(TEXT("Could not find any points to sample."));
+			}
+		};
+
+		Context->TargetsPreloader->StartLoading(Context->GetAsyncManager());
+		return false;
+	}
+
+	PCGEX_POINTS_BATCH_PROCESSING(PCGEx::State_Done)
+
+	Context->MainPoints->StageOutputs();
+
+	return Context->TryComplete();
+}
+
+bool FPCGExSampleVtxByIDElement::CanExecuteOnlyOnMainThread(FPCGContext* Context) const
+{
+	return Context ? Context->CurrentPhase == EPCGExecutionPhase::PrepareData : false;
+}
+
+namespace PCGExSampleVtxByIDs
+{
+	FProcessor::~FProcessor()
+	{
+	}
+
+	void FProcessor::SamplingFailed(const int32 Index)
+	{
+		SamplingMask[Index] = false;
+		const TConstPCGValueRange<FTransform> Transforms = PointDataFacade->GetIn()->GetConstTransformValueRange();
+	}
+
+	bool FProcessor::Process(const TSharedPtr<PCGExMT::FTaskManager>& InAsyncManager)
+	{
+		TRACE_CPUPROFILER_EVENT_SCOPE(PCGExSampleVtxByIDs::Process);
+
+		if (!FPointsProcessor::Process(InAsyncManager)) { return false; }
+
+		PCGEX_INIT_IO(PointDataFacade->Source, PCGExData::EIOInit::Duplicate)
+
+		// Allocate edge native properties
+
+		EPCGPointNativeProperties AllocateFor = EPCGPointNativeProperties::None;
+		if (Context->ApplySampling.WantsApply()) { AllocateFor |= EPCGPointNativeProperties::Transform; }
+		PointDataFacade->GetOut()->AllocateProperties(AllocateFor);
+
+		SamplingMask.SetNumUninitialized(PointDataFacade->GetNum());
+
+		if (!Context->BlendingFactories.IsEmpty())
+		{
+			UnionBlendOpsManager = MakeShared<PCGExDataBlending::FUnionOpsManager>(&Context->BlendingFactories, Context->DistanceDetails);
+			if (!UnionBlendOpsManager->Init(Context, PointDataFacade, Context->TargetFacades)) { return false; }
+			DataBlender = UnionBlendOpsManager;
+		}
+
+		if (!DataBlender)
+		{
+			TSharedPtr<PCGExDataBlending::FDummyUnionBlender> DummyUnionBlender = MakeShared<PCGExDataBlending::FDummyUnionBlender>();
+			DummyUnionBlender->Init(PointDataFacade, Context->TargetFacades);
+			DataBlender = DummyUnionBlender;
+		}
+
+		StartParallelLoopForPoints();
+
+		return true;
+	}
+
+	void FProcessor::ProcessPoints(const PCGExMT::FScope& Scope)
+	{
+		TRACE_CPUPROFILER_EVENT_SCOPE(PCGEx::SampleVtxByID::ProcessPoints);
+
+		PointDataFacade->Fetch(Scope);
+		FilterScope(Scope);
+
+		bool bLocalAnySuccess = false;
+
+		TArray<PCGExData::FWeightedPoint> OutWeightedPoints;
+		TArray<PCGEx::FOpStats> Trackers;
+		DataBlender->InitTrackers(Trackers);
+
+		UPCGBasePointData* OutPointData = PointDataFacade->GetOut();
+		TConstPCGValueRange<FTransform> Transforms = PointDataFacade->GetIn()->GetConstTransformValueRange();
+
+		const TSharedPtr<PCGExSampling::FSampingUnionData> Union = MakeShared<PCGExSampling::FSampingUnionData>();
+		Union->IOSet.Reserve(Context->TargetFacades.Num());
+
+		PCGEX_SCOPE_LOOP(Index)
+		{
+			Union->Reset();
+
+			if (!PointFilterCache[Index])
+			{
+				if (Settings->bProcessFilteredOutAsFails) { SamplingFailed(Index); }
+				continue;
+			}
+
+			FVector WeightedUp = LookAtUpGetter->Read(Index);
+			const PCGExData::FMutablePoint Point = PointDataFacade->GetOutPoint(Index);
+
+			PCGExData::FElement SinglePick(-1, -1);
+
+			// TODO : Find matching Vtx
+
+			if (Union->IsEmpty())
+			{
+				SamplingFailed(Index);
+				continue;
+			}
+
+			DataBlender->ComputeWeights(Index, Union, OutWeightedPoints);
+
+			FTransform WeightedTransform = FTransform::Identity;
+			WeightedTransform.SetScale3D(FVector::ZeroVector);
+
+			double WeightedDistance = Union->GetSqrtWeightAverage();
+
+			// Post-process weighted points and compute local data
+			PCGEx::FOpStats SampleTracker{};
+			for (PCGExData::FWeightedPoint& P : OutWeightedPoints)
+			{
+				const double W = P.Weight;
+
+				SampleTracker.Count++;
+				SampleTracker.Weight += W;
+
+				const FTransform& TargetTransform = Context->TargetFacades[P.IO]->GetIn()->GetTransform(P.Index);
+				const FQuat TargetRotation = TargetTransform.GetRotation();
+
+				WeightedTransform = PCGExBlend::WeightedAdd(WeightedTransform, TargetTransform, W);
+			}
+
+			// Blend using updated weighted points
+			DataBlender->Blend(Index, OutWeightedPoints, Trackers);
+
+			if (SampleTracker.Weight != 0) // Dodge NaN
+			{
+				WeightedUp /= SampleTracker.Weight;
+				WeightedTransform = PCGExBlend::Div(WeightedTransform, SampleTracker.Weight);
+			}
+
+			WeightedUp.Normalize();
+
+			const FVector CWDistance = Origin - WeightedTransform.GetLocation();
+			FVector LookAt = CWDistance.GetSafeNormal();
+
+			FTransform LookAtTransform = PCGExMath::MakeLookAtTransform(LookAt, WeightedUp, Settings->LookAtAxisAlign);
+			if (Context->ApplySampling.WantsApply())
+			{
+				PCGExData::FMutablePoint MutablePoint(OutPointData, Index);
+				Context->ApplySampling.Apply(MutablePoint, WeightedTransform, LookAtTransform);
+			}
+
+			SamplingMask[Index] = !Union->IsEmpty();
+			bLocalAnySuccess = true;
+		}
+
+		if (bLocalAnySuccess) { FPlatformAtomics::InterlockedExchange(&bAnySuccess, 1); }
+	}
+
+	void FProcessor::CompleteWork()
+	{
+		if (UnionBlendOpsManager) { UnionBlendOpsManager->Cleanup(Context); }
+		PointDataFacade->WriteFastest(AsyncManager);
+
+		if (Settings->bTagIfHasSuccesses && bAnySuccess) { PointDataFacade->Source->Tags->AddRaw(Settings->HasSuccessesTag); }
+		if (Settings->bTagIfHasNoSuccesses && !bAnySuccess) { PointDataFacade->Source->Tags->AddRaw(Settings->HasNoSuccessesTag); }
+	}
+
+	void FProcessor::Write()
+	{
+		(void)PointDataFacade->Source->Gather(SamplingMask);
+	}
+
+	void FProcessor::Cleanup()
+	{
+		TPointsProcessor<FPCGExSampleVtxByIDContext, UPCGExSampleVtxByIDSettings>::Cleanup();
+		UnionBlendOpsManager.Reset();
+	}
+}
+
+
+#undef LOCTEXT_NAMESPACE
+#undef PCGEX_NAMESPACE
---
+++ Source/PCGExtendedToolkit/Public/Sampling/PCGExSampleVtxByID.h
@@ -0,0 +1,152 @@
+﻿// Copyright 2025 Timothé Lapetite and contributors
+// Released under the MIT license https://opensource.org/license/MIT/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PCGExGlobalSettings.h"
+
+#include "PCGExPointsProcessor.h"
+#include "PCGExSampling.h"
+#include "PCGExScopedContainers.h"
+#include "Data/Blending/PCGExBlendOpFactoryProvider.h"
+#include "Data/Blending/PCGExDataBlending.h"
+#include "Data/Blending/PCGExUnionOpsManager.h"
+
+#include "PCGExSampleVtxByID.generated.h"
+
+UCLASS(MinimalAPI, BlueprintType, ClassGroup = (Procedural), Category="PCGEx|Sampling", meta=(PCGExNodeLibraryDoc="sampling/vtx-by-id"))
+class UPCGExSampleVtxByIDSettings : public UPCGExPointsProcessorSettings
+{
+	GENERATED_BODY()
+
+public:
+	UPCGExSampleVtxByIDSettings(const FObjectInitializer& ObjectInitializer);
+
+	//~Begin UPCGSettings
+#if WITH_EDITOR
+	PCGEX_NODE_INFOS(SampleVtxByID, "Sample : Vtx by ID", "Sample a cluster vtx by using a stored Vtx ID.");
+	virtual FLinearColor GetNodeTitleColor() const override { return GetDefault<UPCGExGlobalSettings>()->NodeColorSampler; }
+#endif
+
+protected:
+	virtual TArray<FPCGPinProperties> InputPinProperties() const override;
+	virtual FPCGElementPtr CreateElement() const override;
+	//~End UPCGSettings
+
+	//~Begin UPCGExPointsProcessorSettings
+
+public:
+	PCGEX_NODE_POINT_FILTER(PCGExPointFilter::SourcePointFiltersLabel, "Filters", PCGExFactories::PointFilters, false)
+
+	//~End UPCGExPointsProcessorSettings
+
+	/** Whether and how to apply sampled result directly (not mutually exclusive with blending)*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_NotOverridable))
+	FPCGExApplySamplingDetails ApplySampling;
+
+	/** The axis to align transform the look at vector to.*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Outputs", meta=(PCG_Overridable, DisplayName=" ├─ Align"))
+	EPCGExAxisAlign LookAtAxisAlign = EPCGExAxisAlign::Forward;
+
+	/** Up vector source.*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Outputs", meta=(PCG_Overridable, DisplayName=" ├─ Use Up from..."))
+	EPCGExInputValueType LookAtUpSelection = EPCGExInputValueType::Constant;
+
+	/** The attribute or property on selected source to use as Up vector for the look at transform.*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Outputs", meta=(PCG_Overridable, DisplayName=" └─ Up Vector (Attr)", EditCondition="LookAtUpSelection != EPCGExInputValueType::Constant", EditConditionHides))
+	FPCGAttributePropertyInputSelector LookAtUpSource;
+
+	/** The constant to use as Up vector for the look at transform.*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Outputs", meta=(PCG_Overridable, DisplayName=" └─ Up Vector", EditCondition="LookAtUpSelection == EPCGExInputValueType::Constant", EditConditionHides))
+	FVector LookAtUpConstant = FVector::UpVector;
+
+	//
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Tagging", meta=(InlineEditConditionToggle))
+	bool bTagIfHasSuccesses = false;
+
+	/** If enabled, add the specified tag to the output data if at least a single point has been sampled. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Tagging", meta=(EditCondition="bTagIfHasSuccesses"))
+	FString HasSuccessesTag = TEXT("HasSuccesses");
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Tagging", meta=(InlineEditConditionToggle))
+	bool bTagIfHasNoSuccesses = false;
+
+	/** If enabled, add the specified tag to the output data if no points were sampled. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Tagging", meta=(EditCondition="bTagIfHasNoSuccesses"))
+	FString HasNoSuccessesTag = TEXT("HasNoSuccesses");
+
+	//
+
+	/** If enabled, mark filtered out points as "failed". Otherwise, just skip the processing altogether. Only uncheck this if you want to ensure existing attribute values are preserved. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_NotOverridable), AdvancedDisplay)
+	bool bProcessFilteredOutAsFails = true;
+
+	/** If enabled, points that failed to sample anything will be pruned. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_NotOverridable), AdvancedDisplay)
+	bool bPruneFailedSamples = false;
+};
+
+struct FPCGExSampleVtxByIDContext final : FPCGExPointsProcessorContext
+{
+	friend class FPCGExSampleVtxByIDElement;
+
+	TSharedPtr<PCGExData::FMultiFacadePreloader> TargetsPreloader;
+
+	TArray<TSharedRef<PCGExData::FFacade>> TargetFacades;
+
+	TArray<TObjectPtr<const UPCGExBlendOpFactory>> BlendingFactories;
+	TSharedPtr<PCGExDetails::FDistances> DistanceDetails;
+
+	FPCGExApplySamplingDetails ApplySampling;
+};
+
+class FPCGExSampleVtxByIDElement final : public FPCGExPointsProcessorElement
+{
+protected:
+	PCGEX_ELEMENT_CREATE_CONTEXT(SampleVtxByID)
+
+	virtual bool Boot(FPCGExContext* InContext) const override;
+	virtual void PostLoadAssetsDependencies(FPCGExContext* InContext) const override;
+	virtual bool ExecuteInternal(FPCGContext* Context) const override;
+
+	virtual bool CanExecuteOnlyOnMainThread(FPCGContext* Context) const override;
+};
+
+namespace PCGExSampleVtxByIDs
+{
+	class FProcessor final : public PCGExPointsMT::TPointsProcessor<FPCGExSampleVtxByIDContext, UPCGExSampleVtxByIDSettings>
+	{
+		TArray<int8> SamplingMask;
+
+		FVector SafeUpVector = FVector::UpVector;
+		TSharedPtr<PCGExDetails::TSettingValue<FVector>> LookAtUpGetter;
+
+		FPCGExBlendingDetails BlendingDetails;
+
+		TSharedPtr<PCGExDataBlending::FUnionOpsManager> UnionBlendOpsManager;
+		TSharedPtr<PCGExDataBlending::IUnionBlender> DataBlender;
+
+		int8 bAnySuccess = 0;
+
+	public:
+		explicit FProcessor(const TSharedRef<PCGExData::FFacade>& InPointDataFacade)
+			: TPointsProcessor(InPointDataFacade)
+		{
+			DefaultPointFilterValue = true;
+		}
+
+		virtual ~FProcessor() override;
+
+		void SamplingFailed(const int32 Index);
+
+		virtual bool Process(const TSharedPtr<PCGExMT::FTaskManager>& InAsyncManager) override;
+		virtual void ProcessPoints(const PCGExMT::FScope& Scope) override;
+
+		virtual void CompleteWork() override;
+		virtual void Write() override;
+
+		virtual void Cleanup() override;
+	};
+}
