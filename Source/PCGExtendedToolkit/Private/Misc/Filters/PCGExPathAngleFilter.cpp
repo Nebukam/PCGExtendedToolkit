6e3a07ea0ed894b745450f5cd9d1c0b6cabd0bf2
Tim <nebukam@gmail.com>
2025-06-26 12:15:32 -0400

    WIP

---
+++ Source/PCGExtendedToolkit/Private/Misc/Filters/PCGExPathAngleFilter.cpp
@@ -0,0 +1,113 @@
+﻿// Copyright 2025 Timothé Lapetite and contributors
+// Released under the MIT license https://opensource.org/license/MIT/
+
+#include "Misc/Filters/PCGExPathAngleFilter.h"
+
+
+#define LOCTEXT_NAMESPACE "PCGExPathAngleFilterDefinition"
+#define PCGEX_NAMESPACE PCGExPathAngleFilterDefinition
+
+bool UPCGExPathAngleFilterFactory::Init(FPCGExContext* InContext)
+{
+	if (!Super::Init(InContext)) { return false; }
+	Config.Sanitize();
+	return true;
+}
+
+bool UPCGExPathAngleFilterFactory::DomainCheck()
+{
+	return
+		PCGExHelpers::IsDataDomainAttribute(Config.OperandA) &&
+		(Config.CompareAgainst == EPCGExInputValueType::Constant || PCGExHelpers::IsDataDomainAttribute(Config.OperandB)) &&
+		Config.DotComparisonDetails.GetOnlyUseDataDomain() &&
+		!Config.bTransformOperandA && !Config.bTransformOperandB;
+}
+
+TSharedPtr<PCGExPointFilter::FFilter> UPCGExPathAngleFilterFactory::CreateFilter() const
+{
+	return MakeShared<PCGExPointFilter::FPathAngleFilter>(this);
+}
+
+bool UPCGExPathAngleFilterFactory::RegisterConsumableAttributesWithData(FPCGExContext* InContext, const UPCGData* InData) const
+{
+	if (!Super::RegisterConsumableAttributesWithData(InContext, InData)) { return false; }
+
+	FName Consumable = NAME_None;
+	PCGEX_CONSUMABLE_SELECTOR(Config.OperandA, Consumable)
+	PCGEX_CONSUMABLE_CONDITIONAL(Config.CompareAgainst == EPCGExInputValueType::Attribute, Config.OperandB, Consumable)
+	PCGEX_CONSUMABLE_CONDITIONAL(Config.DotComparisonDetails.ThresholdInput == EPCGExInputValueType::Attribute, Config.DotComparisonDetails.ThresholdAttribute, Consumable)
+
+	return true;
+}
+
+bool PCGExPointFilter::FPathAngleFilter::Init(FPCGExContext* InContext, const TSharedPtr<PCGExData::FFacade>& InPointDataFacade)
+{
+	if (!FFilter::Init(InContext, InPointDataFacade)) { return false; }
+
+	DotComparison = TypedFilterFactory->Config.DotComparisonDetails;
+	if (!DotComparison.Init(InContext, InPointDataFacade.ToSharedRef())) { return false; }
+
+	OperandA = PointDataFacade->GetBroadcaster<FVector>(TypedFilterFactory->Config.OperandA, true);
+	if (!OperandA)
+	{
+		PCGEX_LOG_INVALID_SELECTOR_C(InContext, "Operand A", TypedFilterFactory->Config.OperandA)
+		return false;
+	}
+
+	OperandB = TypedFilterFactory->Config.GetValueSettingOperandB();
+	if (!OperandB->Init(InContext, PointDataFacade)) { return false; }
+
+	InTransforms = InPointDataFacade->GetIn()->GetConstTransformValueRange();
+
+	return true;
+}
+
+bool PCGExPointFilter::FPathAngleFilter::Test(const int32 PointIndex) const
+{
+	const FVector B = OperandB->Read(PointIndex).GetSafeNormal();
+	return DotComparison.Test(
+		FVector::DotProduct(
+			TypedFilterFactory->Config.bTransformOperandA ? InTransforms[PointIndex].TransformVectorNoScale(OperandA->Read(PointIndex)) : OperandA->Read(PointIndex),
+			TypedFilterFactory->Config.bTransformOperandB ? InTransforms[PointIndex].TransformVectorNoScale(B) : B),
+		PointIndex);
+}
+
+bool PCGExPointFilter::FPathAngleFilter::Test(const TSharedPtr<PCGExData::FPointIO>& IO, const TSharedPtr<PCGExData::FPointIOCollection>& ParentCollection) const
+{
+	PCGEX_SHARED_CONTEXT(IO->GetContextHandle())
+
+	FVector A = FVector::ZeroVector;
+	FVector B = FVector::ZeroVector;
+
+	if (!PCGExDataHelpers::TryGetSettingDataValue(IO, TypedFilterFactory->Config.CompareAgainst, TypedFilterFactory->Config.OperandB, TypedFilterFactory->Config.OperandBConstant, B)) { return false; }
+	B = B.GetSafeNormal();
+
+	if (!PCGExDataHelpers::TryReadDataValue(IO, TypedFilterFactory->Config.OperandA, A)) { return false; }
+	A = A.GetSafeNormal();
+
+	FPCGExDotComparisonDetails TempComparison = TypedFilterFactory->Config.DotComparisonDetails;
+	PCGEX_MAKE_SHARED(TempFacade, PCGExData::FFacade, IO.ToSharedRef())
+	if (!TempComparison.Init(SharedContext.Get(), TempFacade.ToSharedRef()))
+	{
+		return false;
+	}
+
+	return TempComparison.Test(FVector::DotProduct(A, B), 0);
+}
+
+PCGEX_CREATE_FILTER_FACTORY(Dot)
+
+#if WITH_EDITOR
+FString UPCGExPathAngleFilterProviderSettings::GetDisplayName() const
+{
+	FString DisplayName = PCGEx::GetSelectorDisplayName(Config.OperandA) + TEXT(" ⋅ ");
+
+	if (Config.CompareAgainst == EPCGExInputValueType::Attribute) { DisplayName += PCGEx::GetSelectorDisplayName(Config.OperandB); }
+	else { DisplayName += " (Constant)"; }
+
+	return DisplayName;
+}
+#endif
+
+#undef LOCTEXT_NAMESPACE
+#undef PCGEX_NAMESPACE
---
+++ Source/PCGExtendedToolkit/Public/Misc/Filters/PCGExPathAngleFilter.h
@@ -0,0 +1,132 @@
+﻿// Copyright 2025 Timothé Lapetite and contributors
+// Released under the MIT license https://opensource.org/license/MIT/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PCGExCompare.h"
+#include "PCGExDetailsData.h"
+#include "PCGExFilterFactoryProvider.h"
+#include "UObject/Object.h"
+
+#include "Data/PCGExPointFilter.h"
+#include "PCGExPointsProcessor.h"
+
+
+#include "PCGExPathAngleFilter.generated.h"
+
+UENUM()
+enum class EPCGExPathAngleFilterMode : uint8
+{
+	Curvature = 0 UMETA(DisplayName = "Curvature", Tooltip="Check against the dot product of (Prev to Current) -> (Current to Next)"),
+	Spread    = 1 UMETA(DisplayName = "Spread", Tooltip="Check against the dot product of (Current to Prev) -> (Current to Next)"),
+};
+
+USTRUCT(BlueprintType)
+struct FPCGExPathAngleFilterConfig
+{
+	GENERATED_BODY()
+
+	FPCGExPathAngleFilterConfig()
+	{
+	}
+
+	/** Filter mode */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_NotOverridable))
+	EPCGExPathAngleFilterMode Mode = EPCGExPathAngleFilterMode::Curvature;
+
+	/** What should this filter return when dealing with first points? (if the data doesn't have @Data.IsClosed = true, otherwise wraps) */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta = (PCG_NotOverridable))
+	EPCGExFilterFallback FirstPointFallback = EPCGExFilterFallback::Fail;
+
+	/** What should this filter return when dealing with last points? (if the data doesn't have @Data.IsClosed = true, otherwise wraps) */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta = (PCG_NotOverridable))
+	EPCGExFilterFallback LastPointFallback = EPCGExFilterFallback::Fail;
+
+	/** Dot comparison settings */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta = (PCG_Overridable))
+	FPCGExDotComparisonDetails DotComparisonDetails;
+
+	/** Whether the result of the filter should be inverted or not. Note that this will also invert fallback results! */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta = (PCG_NotOverridable))
+	bool bInvert = false;
+
+	void Sanitize()
+	{
+	}
+};
+
+/**
+ * 
+ */
+UCLASS(MinimalAPI, BlueprintType, ClassGroup = (Procedural), Category="PCGEx|Filter")
+class UPCGExPathAngleFilterFactory : public UPCGExFilterFactoryData
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	FPCGExPathAngleFilterConfig Config;
+
+	virtual bool Init(FPCGExContext* InContext) override;
+
+	virtual bool DomainCheck() override;
+
+	virtual TSharedPtr<PCGExPointFilter::FFilter> CreateFilter() const override;
+	virtual bool SupportsCollectionEvaluation() const override { return false; }
+	virtual bool RegisterConsumableAttributesWithData(FPCGExContext* InContext, const UPCGData* InData) const override;
+};
+
+namespace PCGExPointFilter
+{
+	class FPathAngleFilter final : public FSimpleFilter
+	{
+	public:
+		explicit FPathAngleFilter(const TObjectPtr<const UPCGExPathAngleFilterFactory>& InFactory)
+			: FSimpleFilter(InFactory), TypedFilterFactory(InFactory)
+		{
+			DotComparison = TypedFilterFactory->Config.DotComparisonDetails;
+		}
+
+		const TObjectPtr<const UPCGExPathAngleFilterFactory> TypedFilterFactory;
+
+		bool bIsClosed = false;
+		FPCGExDotComparisonDetails DotComparison;
+		TConstPCGValueRange<FTransform> InTransforms;
+
+		virtual bool Init(FPCGExContext* InContext, const TSharedPtr<PCGExData::FFacade>& InPointDataFacade) override;
+
+		virtual bool Test(const int32 PointIndex) const override;
+
+		virtual ~FPathAngleFilter() override
+		{
+		}
+	};
+}
+
+///
+
+UCLASS(Hidden, MinimalAPI, BlueprintType, ClassGroup = (Procedural), Category="PCGEx|Filter", meta=(PCGExNodeLibraryDoc="filters/filters-points/math-checks/dot-product"))
+class UPCGExPathAngleFilterProviderSettings : public UPCGExFilterProviderSettings
+{
+	GENERATED_BODY()
+
+public:
+	//~Begin UPCGSettings
+#if WITH_EDITOR
+	PCGEX_NODE_INFOS_CUSTOM_SUBTITLE(
+		PathAngleFilterFactory, "Filter : Path Angle", "Creates a filter definition that compares dot value of the direction of a point toward its previous and next points.",
+		PCGEX_FACTORY_NAME_PRIORITY)
+#endif
+	//~End UPCGSettings
+
+	/** Filter Config.*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_Overridable, ShowOnlyInnerProperties))
+	FPCGExPathAngleFilterConfig Config;
+
+	virtual UPCGExFactoryData* CreateFactory(FPCGExContext* InContext, UPCGExFactoryData* InFactory) const override;
+
+#if WITH_EDITOR
+	virtual FString GetDisplayName() const override;
+#endif
+};
