ca9b4cfffaa9c8f35bb816e807ee71d7154848ce
Tim <nebukam@gmail.com>
2025-07-22 14:59:51 -0400

    Update PCGExAttributeHelpers.h

--- Source/PCGExtendedToolkit/Public/Data/PCGExAttributeHelpers.h
+++ Source/PCGExtendedToolkit/Public/Data/PCGExAttributeHelpers.h
@@ -264,7 +264,7 @@ namespace PCGEx
 
 		bool ApplySelector(const FPCGAttributePropertyInputSelector& InSelector, const UPCGData* InData)
 		{
-			static_assert(PCGEx::GetMetadataType<T>() != EPCGMetadataTypes::Unknown);
+			static_assert(PCGEx::GetMetadataType<T>() != EPCGMetadataTypes::Unknown, TEXT("T must be of PCG-friendly type. Custom types are unsupported -- you'll have to static_cast the values."));
 			
 			ProcessingInfos = FAttributeProcessingInfos(InData, InSelector);
 			if (!ProcessingInfos.bIsValid) { return false; }
