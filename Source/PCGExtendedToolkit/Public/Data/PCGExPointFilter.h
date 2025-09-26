bb559c8e32f30fd5457fae8f579bdf9d1c54ced3
Tim <nebukam@gmail.com>
2025-09-26 15:47:22 -0400

    Update

--- Source/PCGExtendedToolkit/Public/Data/PCGExPointFilter.h
+++ Source/PCGExtendedToolkit/Public/Data/PCGExPointFilter.h
@@ -142,7 +142,7 @@ namespace PCGExPointFilter
 {
 	const FName OutputFilterLabel = FName("Filter");
 	const FName OutputColFilterLabel = FName("C-Filter");
-	const FName OutputFilterLabelNode = FName("Node Filter");
+	const FName OutputFilterLabelNode = FName("Vtx Filter");
 	const FName OutputFilterLabelEdge = FName("Edge Filter");
 	const FName SourceFiltersLabel = FName("Filters");
 
